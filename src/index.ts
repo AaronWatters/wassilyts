bf12dda2fe79f4e03db1aa4bd10d539e54b14ee0
Aaron Watters <awatters@flatironinstitute.org>
2025-03-10 10:46:19 -0400

    add tsvector

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@
 // export a name string to test module structure
 export const name = 'wassilyjs';
 
+// expose all tsVector functions
+export * as tsvector from 'tsvector' ;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,20 @@
 
 
 import { describe, expect, it } from 'vitest';
-import { name } from '../src/index';
+import { 
+  name,
+  tsvector
+ } from '../src/index';
 
 describe('Basics', () => {
   it('should return the correct name', () => {
     expect(name).toBe('wassilyjs');
+  });
+  it('should expose tsVector', () => {
+    expect(tsvector).toBeDefined();
+  });
+  it('should expose tsVector functions', () => {
+    const ln = tsvector.vLength([3, 4]);
+    expect(ln).toBe(5);
   });
 });
