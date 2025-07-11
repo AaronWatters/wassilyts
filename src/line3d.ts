fc329917f3997b0db6f959ce59a93193e81a1465
Aaron Watters <awatters@flatironinstitute.org>
2025-07-11 15:44:18 -0400

    use official interface to make a line

--- src/line3d.ts
+++ src/line3d.ts
@@ -13,17 +13,20 @@ export class Line3d extends marking3d.Marking3d {
         super(onFrame3d);
         this.start = start;
         this.end = end;
+        this.stroked();
     }
 
     projectTo2D(): marking.Marking {
         // For simplicity, we can just return a line marking in 2D.
+        //debugger;
         const onFrame3d = this.onFrame3d;
         const startProj = onFrame3d.projection.project(this.start);
         const endProj = onFrame3d.projection.project(this.end);
         // depth as midpoint of projected z values
         this.depthValue = (startProj[2] + endProj[2]) / 2;
         // Create a 2D line marking
-        const result = new line.Line(this.onFrame!, this.to2d(startProj), this.to2d(endProj));
+        //const result = new line.Line(this.onFrame!, this.to2d(startProj), this.to2d(endProj));
+        const result = this.onFrame!.line(this.to2d(startProj), this.to2d(endProj));
         result.styleLike(this);
         return result;
     };
