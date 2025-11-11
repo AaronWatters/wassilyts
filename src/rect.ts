5362ffcacf2f4e4dfdfa0f5fd1ad8f7033ffeffd
Aaron Watters <awatters@flatironinstitute.org>
2025-11-11 10:11:15 -0500

    refactor geometry logic

--- src/rect.ts
+++ src/rect.ts
@@ -6,13 +6,13 @@ import * as marking from './marking';
 // A simple unrotated rectangle with optional offset.
 export class Rectangle extends marking.Marking {
     point: tsvector.Vector;
-    size: tsvector.Vector;
+    size: tsvector.Vector | null;
     offset: tsvector.Vector;
     scaled: boolean;
     constructor(
         frame: frame.Frame, 
         point: tsvector.Vector, 
-        size: tsvector.Vector,
+        size: tsvector.Vector | null = null,
         offset: tsvector.Vector = [0, 0],
         scaled: boolean = true
     ) {
@@ -62,19 +62,7 @@ export class Rectangle extends marking.Marking {
         const path = new Path2D();
         let pixelStart: tsvector.Vector;
         let pixelSize: tsvector.Vector;
-        if (this.scaled) {
-            // offset and size are scaled
-            const start = tsvector.vAdd(this.point, this.offset);
-            const end = tsvector.vAdd(start, this.size);
-            pixelStart = frame.toPixel(start);
-            pixelSize = tsvector.vSub(frame.toPixel(end), pixelStart);
-        } else {
-            // offset and size are unscaled, invert y
-            const offset1 = [this.offset[0], -this.offset[1]];
-            const size1 = [this.size[0], -this.size[1]];
-            pixelStart = tsvector.vAdd(frame.toPixel(this.point), offset1);
-            pixelSize = size1;
-        }
+        ({pixelStart, pixelSize} = this.getPixelStartAndSize());
         //const [px, py] = pixelStart;
         const [sx, sy] = pixelSize;
         //const cstart = frame.diagram.toCartesian(pixelStart);
@@ -90,6 +78,70 @@ export class Rectangle extends marking.Marking {
         frame.addPixelPoint(tsvector.vAdd(pixelStart, pixelSize));
         return path;
     };
+    getPixelStartAndSize(): {pixelStart: tsvector.Vector, pixelSize: tsvector.Vector} {
+        debugger;
+        let size = this.size;
+        if (size === null) {
+            throw new Error("Rectangle size is not defined.");
+        }
+        if (!this.isLive()) {
+            throw new Error("Rectangle is not attached to a frame.");
+        }
+        const frame = this.onFrame!;
+        let pixelStart: tsvector.Vector;
+        let pixelSize: tsvector.Vector;
+        let point = this.point;
+        let offset = this.offset;
+        let lowerLeftCartesian: tsvector.Vector;
+        let upperLeftCartesian: tsvector.Vector;
+        if (this.scaled) {
+            // offset and size are scaled.
+            let lowerleftFrame = tsvector.vAdd(point, offset);
+            let upperleftFrame = tsvector.vAdd(lowerleftFrame, size);
+            lowerLeftCartesian = frame.toPixel(lowerleftFrame);
+            upperLeftCartesian = frame.toPixel(upperleftFrame);
+        } else {
+            // offset and size are unscaled
+            let pointCartesian = frame.toPixel(point);
+            lowerLeftCartesian = tsvector.vAdd(pointCartesian, offset);
+            upperLeftCartesian = tsvector.vAdd(lowerLeftCartesian, size);
+        }
+        let [llx, lly] = lowerLeftCartesian;
+        let [ulx, uly] = upperLeftCartesian;
+        pixelStart = [Math.min(llx, ulx), Math.min(lly, uly)];
+        let pixelEnd = [Math.max(llx, ulx), Math.max(lly, uly)];
+        pixelSize = tsvector.vSub(pixelEnd, pixelStart);
+        return {pixelStart, pixelSize};
+    };
+    getPixelStartAndSize0(): {pixelStart: tsvector.Vector, pixelSize: tsvector.Vector} {
+        // historical...
+        let size = this.size;
+        if (size === null) {
+            throw new Error("Rectangle size is not defined.");
+        }
+        if (!this.isLive()) {
+            throw new Error("Rectangle is not attached to a frame.");
+        }
+        const frame = this.onFrame!;
+        let pixelStart: tsvector.Vector;
+        let pixelSize: tsvector.Vector;
+        if (this.scaled) {
+            // offset and size are scaled
+            const start = tsvector.vAdd(this.point, this.offset);
+            const end = tsvector.vAdd(start, size);
+            pixelStart = frame.toPixel(start);
+            const endPixel = frame.toPixel(end);
+            pixelSize = tsvector.vSub(endPixel, pixelStart);
+        } else {
+            // offset and size are unscaled, invert y
+            const offset1 = [this.offset[0], -this.offset[1]];
+            const size1 = [size[0], -size[1]];
+            const pointPixel = frame.toPixel(this.point);
+            pixelStart = tsvector.vAdd(pointPixel, offset1);
+            pixelSize = size1;
+        }
+        return {pixelStart, pixelSize};
+    }
 };
 
 export function Square(
