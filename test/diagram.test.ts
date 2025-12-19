06cb893373ec77f4403808db561427aa4578546d
Aaron Watters <awatters@flatironinstitute.org>
2025-12-19 09:11:07 -0500

    tests for overlap

--- test/diagram.test.ts
+++ test/diagram.test.ts
@@ -33,4 +33,26 @@ describe('diagram module', () => {
     expect(diag.stats.minxy).toEqual([2, 4]);
     expect(diag.stats.maxxy).toEqual([3, 6]);
   });
+  it('should reset stats', () => {
+    const container = document.createElement('div');
+    const diag = new diagram.Diagram(container, 120, 100);
+    diag.addPoint([3, 4]);
+    diag.addxy(2, 6);
+    diag.resetStats();
+    expect(diag.stats.minxy).toBeNull();
+    expect(diag.stats.maxxy).toBeNull();
+  });
+  it('should detect overlapping stats', () => {
+    const stats1 = new diagram.CanvasStats();
+    stats1.addxy(0, 2);
+    stats1.addxy(2, 0);
+    const stats2 = new diagram.CanvasStats();
+    stats2.addxy(1, 1);
+    stats2.addxy(3, 3);
+    expect(stats1.overlaps(stats2)).toBe(true);
+    const stats3 = new diagram.CanvasStats();
+    stats3.addxy(3, 3);
+    stats3.addxy(4, 4);
+    expect(stats1.overlaps(stats3)).toBe(false);
+  });
 });
